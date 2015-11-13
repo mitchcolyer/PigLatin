9fbc068ebcbde2e7a4ea70f6be12e618b662f382
Mitch Colyer <Mitch@Mitchs-MacBook-Air.local>
2015-11-12 22:04:50 -0700

    Uploading attempt at a multi-word converter

--- PigLatin.java
+++ PigLatin.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class PigLatin {
@@ -7,14 +9,43 @@ public class PigLatin {
         Scanner readInput = new Scanner(System.in);
         System.out.println("Enter any word you want: ");
         String input = readInput.nextLine();
+        String delims = "[ ]+";
+        String[] tokens = input.split(delims);
+        ArrayList al = new ArrayList();
 
-        int length = input.length() - 1;
-        char v = Character.toLowerCase(input.charAt(0));
-        String sub = input.substring(1,length);
 
-        String pigLatinWord = sub + v + "ay";
 
-        System.out.println(pigLatinWord);
+        for (int i = 0; i <tokens.length; i++) {
+            al.add(tokens);
+        }
+        Iterator iter = al.iterator();
+        while (iter.hasNext()) {
+            String element = (String) iter.next();
+
+            int length = element.length();
+            char v = Character.toLowerCase(element.charAt(0));
+            String sub = element.substring(1,length);
+
+            String pigLatinWord = sub + v + "ay";
+            System.out.println(pigLatinWord);
+
+        }
+
+/*
+            int length = input.length();
+            char v = Character.toLowerCase(input.charAt(0));
+            String sub = input.substring(1, length);
+
+            String pigLatinWord = sub + v + "ay";
+
+
+
+            System.out.println(pigLatinWord);
+
+            System.out.println(tokens[i]);
+            System.out.println(al.toString());
+*/
+
 
     }
 }
